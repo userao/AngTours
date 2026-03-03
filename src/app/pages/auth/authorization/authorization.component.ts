a3373962b150fae7d3488dc9b4d88e0105548a6a
Andrey O <ostapenko2121@gmail.com>
2026-03-03 13:01:34 +0700

    added auth page

---
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization',
+  imports: [],
+  templateUrl: './authorization.component.html',
+  styleUrl: './authorization.component.scss',
+})
+export class AuthorizationComponent { }
