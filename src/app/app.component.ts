5924a42a3e4cfe943fe3057939a6ddbaf4457918
Andrey O <ostapenko2121@gmail.com>
2026-03-03 14:58:05 +0700

    added auth route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
 
 @Component({
   selector: 'app-root',
