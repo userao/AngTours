5d5376aa44eb70f04638b56cad7f3df66e6ad9d8
Andrey O <ostapenko2121@gmail.com>
2026-03-10 13:10:18 +0700

    added index route, layout and basic styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+  },
   {
     path: 'auth',
     component: AuthComponent,
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [AsideComponent, HeaderComponent, FooterComponent, RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
