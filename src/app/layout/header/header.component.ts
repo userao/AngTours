7927dec089cc6a804f717ad8c5cb3554cc0cfb48
Andrey O <ostapenko2121@gmail.com>
2026-03-13 11:05:11 +0700

    username rendering in header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: "app-header",
+    imports: [],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
-
+    userName: string = localStorage.getItem("user");
 }
