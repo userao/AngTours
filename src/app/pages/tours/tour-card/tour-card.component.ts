1d5d632164e0613852da459451c153bfde065d52
Andrey O <ostapenko2121@gmail.com>
2026-03-23 10:15:25 +0700

    images compressed
    changed tour cards styling
    
    changed tour cards styling

--- src/app/pages/tours/tour-card/tour-card.component.ts
+++ src/app/pages/tours/tour-card/tour-card.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from "@angular/core";
 import { ITour } from "../../../models/tour";
 import { MatCardModule } from "@angular/material/card";
+<<<<<<< HEAD
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
     selector: "app-tour-card",
-    imports: [MatCardModule],
+    imports: [MatCardModule, MatButtonModule],
+=======
+import { TextLimitingPipe } from "../../../pipes/text-limiting.pipe";
+
+@Component({
+    selector: "app-tour-card",
+    imports: [MatCardModule, TextLimitingPipe],
+>>>>>>> 6c1e66b (changed tour cards styling)
     templateUrl: "./tour-card.component.html",
     styleUrl: "./tour-card.component.scss",
 })
