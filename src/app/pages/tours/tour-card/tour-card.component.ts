82ec152c462eb96d525c1eb6a5f825cb6520dce4
Andrey O <ostapenko2121@gmail.com>
2026-03-23 11:51:17 +0700

    changed tour cards styling

--- src/app/pages/tours/tour-card/tour-card.component.ts
+++ src/app/pages/tours/tour-card/tour-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from "@angular/core";
 import { ITour } from "../../../models/tour";
 import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
     selector: "app-tour-card",
-    imports: [MatCardModule],
+    imports: [MatCardModule, MatButtonModule],
     templateUrl: "./tour-card.component.html",
     styleUrl: "./tour-card.component.scss",
 })
