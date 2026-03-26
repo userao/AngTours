2efd1bbc4bb86d7371254fa06547a859c8fab900
Andrey O <ostapenko2121@gmail.com>
2026-03-26 11:03:11 +0700

    added form to order page

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,18 +1,29 @@
 import { Component, inject, OnInit } from "@angular/core";
-import { DatePipe } from "@angular/common";
+import { DatePipe, NgClass } from "@angular/common";
 import { TourService } from "../../services/tour.service";
 import { ITour } from "../../models/tour";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+
 
 @Component({
     selector: "app-order",
-    imports: [DatePipe],
+    imports: [DatePipe, FormsModule, MatButtonModule, NgClass],
     templateUrl: "./order.component.html",
     styleUrl: "./order.component.scss",
 })
 export class OrderComponent implements OnInit {
     private tourService = inject(TourService);
     tour: ITour = null;
+    submitted = false;
+    name: string = '';
+    email: string = '';
+
     ngOnInit(): void {
         this.tour = this.tourService.getSavedTour();
+    }
+    handleSubmit(): void {
+        this.submitted = true;
+        console.log(`Тур ${this.tour.name} забронирован!`);
     }
 }
